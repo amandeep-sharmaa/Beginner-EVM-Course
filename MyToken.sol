7ff164686aa2025d46ec8a861be24aa83505f56d
Amandeep Sharma <143179675+amandeep-sharmaa@users.noreply.github.com>
2024-05-23 12:17:53 +0530

    Project_Module3_ETH_Beginner

---
+++ MyToken.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract MyToken {
+    
+    //public variable
+    string public token_name="AmanDeep";
+    string public token_abbr="AD";
+    uint public total_supply=0;      
+
+    //Mapping of Address to balance
+    mapping(address => uint) public balances;  
+    
+    //function to increase the total supply and update 
+    //the balance of "sender" address
+    function mint(address account, uint value) public {
+        total_supply = total_supply + value;          
+        balances[account] = balances[account] + value;   
+    }
+
+    //burn function takes the address and value and then
+    //deduct the value from "sender" address
+    function burn(address account, uint value) public {
+      if(balances[account] >= value){
+        total_supply = total_supply- value;          
+        balances[account] =balances[account]- value;    
+      }  
+    }
+}
